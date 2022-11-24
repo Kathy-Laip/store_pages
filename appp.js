7156a92d11e1bb65aca3652391c7372ed100cc00
Kathy <yekaterina.shlyapnikova@yandex.ru>
2022-11-24 13:35:34 +0300

    sERVER

--- appp.js
+++ appp.js
@@ -1,19 +1,19 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 // http.createServer().listen(3000);
 http.createServer(function(request, response){
-    console.log(request.url)
-    console.log(request.method)
-    console.log(request.headers['user-agent'])
+    console.log(request.url);
+    console.log(request.method);
+    console.log(request.headers['user-agent']);
 
     response.setHeader('Content-type', 'text/html;charset=utf-8;')
 
     if(request.url == '/'){
         response.end('Main <h2>Hello</h2> Привет мир');
     } 
-    else if(request.url == '/online-shop.html'){
-        let mainPage = fs.readFileSync('online-shop.html')
-        console.log(mainPage)
+    else if (request.url == '/online-shop.html'){
+        let mainPage = fs.readFileSync('online-shop.html');
+        console.log(mainPage);
         response.end(mainPage);
     }
 }).listen(3000);
