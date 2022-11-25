b669be00562a1dc25d96bfb8024ea3a5d7a7ee07
Kathy <yekaterina.shlyapnikova@yandex.ru>
2022-11-25 13:23:33 +0300

    app

--- appp.js
+++ appp.js
@@ -1,6 +1,7 @@
-let express = require('express');
-let app = express();
-app.use(express.static('public'));
+let express = require('express')
+let app = express()
+app.use(express.static('public'))
+app.set('view engine', 'pug')
 
 app.listen(3000, function(){
     console.log('node express work on 3000')
