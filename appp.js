bd68fe94c46eb96ce59c88d8e5709367e9b8a970
Kathy <yekaterina.shlyapnikova@yandex.ru>
2022-12-01 00:32:45 +0300

    dasdasd

--- appp.js
+++ appp.js
@@ -9,9 +9,6 @@ let con = mysql.createConnection({
     database: 'shop1'
 });
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
 con.connect(function(err){
     if(err) throw err
     console.log('Connected!')
@@ -29,24 +26,6 @@ app.get('/online-shop.html', function(req, res){
     res.sendFile('online-shop.html', {root : __dirname + '/public'})
 });
 
-// con.query(
-//     'SELECT * FROM product',
-//     function(error, result){
-//         if (error) 
-//         {
-//             throw error;
-//         }
-//         let us = {}
-//         for(let i = 0; i < result.length; i++){
-//             us[result[i]['id']] = result[i];
-//         }
-//         console.log(JSON.parse(JSON.stringify(us)))
-//     }
-// );
-
-global.document = new JSDOM(this.html).window.document;
-let datType = document.querySelector('#textType')
-
 con.query(
     'SELECT id, type FROM product_type WHERE id = 1',
     function(error, result){
@@ -57,9 +36,8 @@ con.query(
         }
         // console.log(JSON.parse(JSON.stringify(type['1'])))
         // datType.innerHTML = JSON.parse(JSON.stringify(type['1']))
-        console.log(type['1'])
         app.post('/catalog.html', (req,res) => {
-            res.send({type1: type['1']})
+            res.send(type['1'])
         })
     }
 )
--- public/catalog.js
+++ public/catalog.js
@@ -1,10 +1,9 @@
 let datType = document.querySelector('#textType')
-// datType.innerHTML = {{typeD}}
 
 var request = new XMLHttpRequest()
 request.open('POST', '/catalog.html')
 request.onload = function(){
-    datType.textContent = request.response['type1']
+    datType.textContent = request.response
 };
 
 request.send()
