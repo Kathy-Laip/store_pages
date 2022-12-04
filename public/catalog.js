fe65f97975088ea01aa968ad3e156ea9ac4daaf6
Kathy <yekaterina.shlyapnikova@yandex.ru>
2022-12-05 02:19:01 +0300

    sldfksd

--- public/catalog.js
+++ public/catalog.js
@@ -117,10 +117,10 @@ async function pr1(data){
     console.log(mas)
     for(let i = 0; i < products.length; i++){
         if(mas[i].length === 5){
-            products[i].innerHTML = mas[i][1] + '<br/>' + 'Бренд: ' + mas[i][2] + '<br/>' + 'Цвет: ' + mas[i][3] + '<br/>' + 'Тип: ' + mas[i][4].replace('"', '').replace('}', '')
+            products[i].innerHTML = mas[i][1].replace('_',' ') + '<br/>' + 'Бренд: ' + mas[i][2] + '<br/>' + 'Цвет: ' + mas[i][3] + '<br/>' + 'Тип: ' + mas[i][4].replace('"', '').replace('}', '')
         } 
         else if(mas[i].length === 4){
-            products[i].innerHTML = mas[i][1] + '<br/>' + 'Бренд: ' + mas[i][2] + '<br/>' + 'Тип: ' + mas[i][3].replace('"', '')
+            products[i].innerHTML = mas[i][1].replace('_', ' ') + '<br/>' + 'Бренд: ' + mas[i][2] + '<br/>' + 'Тип: ' + mas[i][3].replace('"', '')
         }
     }
     
